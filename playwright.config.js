c3d083d4f5f4cbd7f77b59f4c5e4d58643602ba4
dimitar-techstarter <147836142+dimitar-techstarter@users.noreply.github.com>
2024-03-01 13:00:20 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ const config = {
   webServer: {
     command: 'npm run start',
     url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI
+    reuseExistingServer: process.env.CI
   },
 };
 
